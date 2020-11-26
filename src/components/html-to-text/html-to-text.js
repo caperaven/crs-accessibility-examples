60867bf477457bc05445f749dee730c2e42200de
caperaven <caperaven@gmail.com>
2020-11-26 14:08:39 +0200

    initial

--- src/components/html-to-text/html-to-text.js
+++ src/components/html-to-text/html-to-text.js
@@ -6,7 +6,15 @@ export class HtmlToText extends HTMLElement {
     async initElement() {
         const link = this.getAttribute("src");
         const response = await fetch(link).then((response) => response.text());
-        this.innerText = response;
+        await this.createContent(response)
+    }
+
+    async createContent(text) {
+        const pre = document.createElement("pre");
+        const code = document.createElement("code");
+        code.textContent = text;
+        pre.appendChild(code);
+        this.appendChild(pre);
     }
 
 }
